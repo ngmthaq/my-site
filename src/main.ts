fd0927516c2af42ec0ea29987ba45ed8e200b077
Nguyen Manh Thang <ngmthaq.20@gmail.com>
2025-01-04 09:12:41 +0700

    feat: install bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { pinia } from "./stores";
 import { i18n } from "./i18n";
 import { queryClient } from "./services";
 import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./assets/scss/index.scss";
 
 const app = createApp(App);

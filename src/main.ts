524bad0555a106551b8a25b032bab0762e8a2d8b
Nguyen Manh Thang <ngmthaq.20@gmail.com>
2025-01-04 09:07:48 +0700

    fix: remove primevue

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { router } from "./router";
 import { pinia } from "./stores";
 import { i18n } from "./i18n";
 import { queryClient } from "./services";
-import { theme, themeOptions } from "./theme";
 import App from "./App.vue";
-import "primeicons/primeicons.css";
 import "./assets/scss/index.scss";
 
 const app = createApp(App);
@@ -13,5 +11,4 @@ app.use(router);
 app.use(pinia);
 app.use(i18n);
 app.use(queryClient);
-app.use(theme, themeOptions);
 app.mount("#app");

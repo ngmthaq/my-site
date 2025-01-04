af24a4c6a4c4fc58f72016ce045f49098bc6918e
Nguyen Manh Thang <ngmthaq.20@gmail.com>
2025-01-04 07:19:00 +0700

    feat: init routes

--- src/router/route-names.ts
+++ src/router/route-names.ts
@@ -1,7 +1,18 @@
 export const routeNames = {
-  dashboard: {},
+  dashboard: {
+    about: "dashboard-about",
+    home: "dashboard-home",
+    jobDetail: "dashboard-job-detail",
+    login: "dashboard-login",
+    project: "dashboard-project",
+    techstack: "dashboard-techstack",
+    wedding: "dashboard-wedding",
+  },
   root: {
-    home: "home",
     about: "about",
+    home: "home",
+    project: "project",
+    techstack: "techstack",
+    wedding: "wedding",
   },
 };
--- src/router/route-records.ts
+++ src/router/route-records.ts
@@ -4,12 +4,75 @@ import { routeNames } from "./route-names";
 export const routeRecords: RouteRecordRaw[] = [
   {
     path: "/",
-    name: routeNames.root.home,
-    component: () => import("../views/root/HomeView.vue"),
+    component: () => import("../components/layouts/PublicLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: routeNames.root.home,
+        component: () => import("../views/root/HomeView.vue"),
+      },
+      {
+        path: "about",
+        name: routeNames.root.about,
+        component: () => import("../views/root/AboutView.vue"),
+      },
+      {
+        path: "project",
+        name: routeNames.root.project,
+        component: () => import("../views/root/ProjectView.vue"),
+      },
+      {
+        path: "techstack",
+        name: routeNames.root.techstack,
+        component: () => import("../views/root/TechstackView.vue"),
+      },
+      {
+        path: "wedding",
+        name: routeNames.root.wedding,
+        component: () => import("../views/root/WeddingView.vue"),
+      },
+    ],
   },
   {
-    path: "/about",
-    name: routeNames.root.about,
-    component: () => import("../views/root/AboutView.vue"),
+    path: "/login",
+    component: () => import("../components/layouts/GuestLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: routeNames.dashboard.login,
+        component: () => import("../views/dashboard/LoginView.vue"),
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    component: () => import("../components/layouts/AuthLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: routeNames.dashboard.home,
+        component: () => import("../views/dashboard/HomeView.vue"),
+      },
+      {
+        path: "about",
+        name: routeNames.dashboard.about,
+        component: () => import("../views/dashboard/AboutView.vue"),
+      },
+      {
+        path: "project",
+        name: routeNames.dashboard.project,
+        component: () => import("../views/dashboard/ProjectView.vue"),
+      },
+      {
+        path: "techstack",
+        name: routeNames.dashboard.techstack,
+        component: () => import("../views/dashboard/TechstackView.vue"),
+      },
+      {
+        path: "wedding",
+        name: routeNames.dashboard.wedding,
+        component: () => import("../views/dashboard/WeddingView.vue"),
+      },
+    ],
   },
 ];
